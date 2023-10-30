8352f6964627f8e86f121ee0eef5a88c89e7456a
Mirko Ponce <mko.bartender@gmail.com>
2023-10-30 14:33:24 -0300

    Completada algunas actividades del pre-bootcam, incluido avances de la clase de CSS de hoy

---
+++ CodingDojo/Mirko_Ponce_JS_Jugueteo.js
@@ -0,0 +1,9 @@
+var alturaNiño = 68;
+function muestraSiElNiñoPuedeSubirALaMontañaRusa(alturaNiño){
+    if(alturaNiño >52){
+        console.log("!Súbete, chico!");
+    }
+    else {
+        console.log("Lo siento, chico. Tal vez el próximo año");
+    }
+}
