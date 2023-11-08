20dc3945c0a0bde8189d480f0812f54cfcfa1eb5
Mirko Ponce <mko.bartender@gmail.com>
2023-11-08 10:49:01 -0300

    Añadiendo contenidos clase 8 de nov

--- Front-End/core4_Pagina_de_perfil_Mirko_Ponce/js/script.js
+++ Front-End/core4_Pagina_de_perfil_Mirko_Ponce/js/script.js
@@ -24,3 +24,23 @@ function cambio_img(elemento_img){
 function restaura_img(elemento_img){
     elemento_img.src = "/Coding_Dojo_Bootcamp_Publico/Front-End/core4_Pagina_de_perfil_Mirko_Ponce/img/images/user-square.png"
 }
+
+function editar_perfil() {
+    var elemento_nombre = document.querySelector("#nombre");
+
+    //Obtener el elemento
+    var input = document.getElementById("input_nombre");
+    if(input.value == ""){
+        elemento_nombre.innerText = "elMk po";
+    } else {
+        elemento_nombre.innerText = input.value;
+    }    
+
+    document.querySelector(".location").innerHTML = '<img class="icons" src="/Coding_Dojo_Bootcamp_Publico/Front-End/core4_Pagina_de_perfil_Mirko_Ponce/img/icons/map-marker.png" alt="map marker">Playa Ancha';
+
+    document.querySelector('.cardBody h5').innerHTML = 'Animador Digital <span>|</span> Desarrollador Web Front-End<span>|</span> Técnico legend'
+
+    elemento_nombre.style.color = "white";
+    elemento_nombre.style.backgroundColor = "blue";
+
+}
