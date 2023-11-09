628a5f95d9c6910df8470251f19f8f3401672c55
Mirko Ponce <mko.bartender@gmail.com>
2023-11-09 15:06:09 -0300

    Tiempo Dojo terminado

--- Front-End/p16_Tiempo_Dojo_Mirko_Ponce/js/script.js
+++ Front-End/p16_Tiempo_Dojo_Mirko_Ponce/js/script.js
@@ -4,8 +4,32 @@ function alerta(elemento){
     alert(`Cargando reporte meteorológico de ${lugar}`);
 }
 
-function eliminar_cookie() {
+function eliminarCookie() {
     var cookie = document.querySelector(".cookie");
     // console.log(cookie);
     cookie.remove();
+}
+
+function cambiarTemperatura() {
+    var CoF = document.querySelector("select").value;
+    console.log(CoF);
+
+    for(let i=0; i<8; i++){
+        var tempElement = document.querySelector('#temp'+i);
+        var temperature = tempElement.innerText;
+        temperature = parseInt(temperature); //Transforma texto a numero
+        console.log(temperature);
+
+        var newTemp = 0;
+        if(CoF === "C°") {
+            //F -> C
+            newTemp = Math.round((temperature - 32) * 5 / 9);
+        } else {
+            //C -> F
+            newTemp = Math.round((temperature * 9 / 5) + 32);
+        }
+
+        tempElement.innerText = (`${newTemp}°`);        
+    }
+    
 }
