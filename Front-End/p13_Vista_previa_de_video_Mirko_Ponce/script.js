b64edcdd045d9a4a00d4bc559aa802176f05123b
Mirko Ponce <mko.bartender@gmail.com>
2023-11-10 16:49:00 -0300

    panic update

--- Front-End/p13_Vista_previa_de_video_Mirko_Ponce/script.js
+++ Front-End/p13_Vista_previa_de_video_Mirko_Ponce/script.js
@@ -2,8 +2,7 @@ console.log("page loaded...");
 
 function playVideo(element) {
     element.play();
-    // var video_tag = document.querySelector(".mainVideo");
-    // console.log(`Este es el tag seleccionado: ${video_tag}`);
+    
 }
 
 function pauseVideo(element) {
