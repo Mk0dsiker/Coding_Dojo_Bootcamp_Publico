63b40710e93e50f9f2be04aa8708f84dc79b134e
Mirko Ponce <mko.bartender@gmail.com>
2023-11-10 15:24:49 -0300

    Añadiendo y finalizando actividad de práctica n° 13

--- Front-End/p13_Vista_previa_de_video_Mirko_Ponce/script.js
+++ Front-End/p13_Vista_previa_de_video_Mirko_Ponce/script.js
@@ -1,1 +1,12 @@
 console.log("page loaded...");
+
+function playVideo(element) {
+    element.play();
+    // var video_tag = document.querySelector(".mainVideo");
+    // console.log(`Este es el tag seleccionado: ${video_tag}`);
+}
+
+function pauseVideo(element) {
+    element.pause();
+    element.currentTime = 0;
+}
